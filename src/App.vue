727318babc434b8e0a87098bbf0222917d937e82
Wei Wang <waix@Msn.com>
2018-09-08 00:02:45 -0700

    made timeline work

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png">
-        <timeline :items="vueState.data1"></timeline>
-        <timeline :items="vueState.data2"></timeline>
+        <timeline></timeline>
     </div>
 </template>
 
@@ -11,76 +10,6 @@
 
     export default {
         name: "app",
-        data: function() {
-            return {
-                vueState: {
-                    data1: [{
-                            id: 1,
-                            content: 'item 1',
-                            start: '2013-04-20'
-                        },
-                        {
-                            id: 2,
-                            content: 'item 2',
-                            start: '2013-04-14'
-                        },
-                        {
-                            id: 3,
-                            content: 'item 3',
-                            start: '2013-04-18'
-                        },
-                        {
-                            id: 4,
-                            content: 'item 4',
-                            start: '2013-04-16',
-                            end: '2013-04-19'
-                        },
-                        {
-                            id: 5,
-                            content: 'item 5',
-                            start: '2013-04-25'
-                        },
-                        {
-                            id: 6,
-                            content: 'item 6',
-                            start: '2013-04-27'
-                        }
-                    ],
-                    data2: [{
-                            id: 1,
-                            content: 'item 11',
-                            start: '2017-04-20'
-                        },
-                        {
-                            id: 2,
-                            content: 'item 12',
-                            start: '2017-04-14'
-                        },
-                        {
-                            id: 3,
-                            content: 'item 13',
-                            start: '2017-04-18'
-                        },
-                        {
-                            id: 4,
-                            content: 'item 14',
-                            start: '2017-04-16',
-                            end: '2017-04-19'
-                        },
-                        {
-                            id: 5,
-                            content: 'item 15',
-                            start: '2017-04-25'
-                        },
-                        {
-                            id: 6,
-                            content: 'item 16',
-                            start: '2017-04-27'
-                        }
-                    ]
-                }
-            }
-        },
         components: {
             Timeline
         }
@@ -89,7 +18,7 @@
 
 <style>
     #app {
-        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        font-family: "Myriad Prossssssss", Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -4,18 +4,97 @@
 </template>
 
 <script>
+import vis from "vis";
+import {
+    rawdata
+} from "../rawdata";
 
-import vis from 'vis'
+const raw =
+    rawdata["play-tracking"]["build-a5501213ffa929746b625d0ca300490e"][
+        "9-2-2018"
+    ]["ae17786a-e985-4832-813a-5b4911dc41c2"];
+
+const convertData = function (rawObject) {
+    return Object.keys(rawObject).map(key => rawObject[key]);
+};
+
+const createTimeline = function () {
+    var items = [];
+    // Process Sequences
+    const sequences = convertData(raw.sequences);
+    const sequenceItems = sequences.map(element => ({
+        content: "Sequence " + (element.index + 1),
+        start: element.start,
+        // style: 'background-color: ' + '#'+((1<<24)*Math.random()|0).toString(16) + ';',
+        end: element.end ? element.end : element.start + 150,
+        type: "background"
+    }));
+    items = items.concat(sequenceItems);
+
+    // Process Chunks
+    const chunks = convertData(raw.chunks);
+    const chunkItems = chunks.map(element => ({
+        content: element.name,
+        start: element.start,
+        end: element.end,
+        style: "background-color: " +
+            "#" +
+            (((1 << 24) * Math.random()) | 0).toString(16) +
+            ";",
+        type: element.end ? "range" : "point"
+    }));
+    items = items.concat(chunkItems);
+
+    // Process Timelines
+    const timelines = convertData(raw.timelines);
+    const timelineItems = timelines.map(element => ({
+        content: element.name,
+        start: element.start,
+        end: element.end
+    }));
+    items = items.concat(timelineItems);
+
+    // Process Events
+    const events = convertData(raw.events);
+    const eventItems = events.map(element => ({
+        content: element.name,
+        start: element.time,
+        type: "point"
+    }));
+    items = items.concat(eventItems);
+
+    // Process Errors
+    const errorData = convertData(raw.errors);
+    const errorItems = errorData.map(element => ({
+        start: element.time,
+        content: "Error",
+        style: "color: red;",
+        title: element.name,
+        type: "point"
+    }));
+    //timelineItems = timelineItems.concat(errorItems)
+
+    var items = new vis.DataSet(items);
+
+    var options = {
+        horizontalScroll: true,
+        min: 0,
+        max: 680,
+        editable: false,
+        margin: {
+            item: 10, // minimal margin between items
+            axis: 5 // minimal margin between items and the axis
+        },
+        orientation: "top"
+    };
+
+    var groups = this.groups;
+    var timeline = new vis.Timeline(this.$el, items, groups, options);
+};
 
 export default {
-    props: ["items", "groups", "options"],
-    mounted: function () {
-        var items = new vis.DataSet(this.items);
-        var options = this.options;
-        var groups = this.groups;
-        var timeline = new vis.Timeline(this.$el, items, groups, options);
-    }
-}
+    mounted: createTimeline
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
